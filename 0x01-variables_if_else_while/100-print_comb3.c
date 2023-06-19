bde85482c8f048b31ecd7445f11750750d1e8fce
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-19 16:25:00 +0000

    Done Combination!

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,31 +10,26 @@
 
 int main(void)
 {
-	int ch1 = 0, ch2, CountLoop = 1, i = 0;
+	int ch1 = 0, ch2;
 
 	while (ch1 <= 9)
 	{
 		ch2 = 0;
 		while (ch2 <= 9)
 		{
-			if (i < CountLoop)
+			if (ch1 != ch2 && ch2 > ch1)
 			{
-				i++;
-				ch2++;
-				continue;
-			}
-			putchar(ch1 + '0');
-			putchar(ch2 + '0');
-			if (ch1 != 8)
-			{
-				putchar(',');
-				putchar(' ');
+				putchar(ch1 + '0');
+				putchar(ch2 + '0');
+				if (ch1 + ch2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			ch2++;
 		}
 		ch1++;
-		CountLoop++;
-		i = 0;
 	}
 	putchar('\n');
 	return (0);
