337064d9c9f9375486e61aefb67e0df4605d3562
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-20 18:56:11 +0000

    DONE 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Function to print till 98.
+ * @n: number to start counting from.
+ *
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	else
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	printf("98\n");
+}
