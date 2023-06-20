8f51cb61361a9ddffc928fe1133e5d85a8b1e634
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-20 15:58:23 +0000

    DONE 24 time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Function to print 24 hour.
+*/
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		if (i < 2)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+		else
+		{
+			for (j = 0; j < 4; j++)
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
