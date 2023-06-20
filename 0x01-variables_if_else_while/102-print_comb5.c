7ed62fbe2afe85ee87c2a16ef875a3bceb1ef98e
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-20 12:40:48 +0000

    DONE PUTCHAR

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function point
+ *
+ * Description: print 0, 1 - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int ch1 = 0, ch2, ch3, ch4;
+
+	while (ch1 <= 9)
+	{
+		ch2 = 0;
+		while (ch2 <= 9)
+		{
+			ch3 = 0;
+			while (ch3 <= 9)
+			{
+				if (ch1 != ch2 && ch2 != ch3 && ch2 > ch1 && ch3 > ch2 && ch3 > ch1)
+				{
+					putchar(ch1 + '0');
+					putchar(ch2 + '0');
+					putchar(ch3 + '0');
+					if (ch1 + ch2 + ch3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				ch3++;
+			}
+			ch2++;
+		}
+		ch1++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Documentation: Documentation for all upcoming projects.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+	return (0);
+}
