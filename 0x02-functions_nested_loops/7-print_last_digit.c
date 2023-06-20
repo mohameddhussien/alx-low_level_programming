34397ccb2e1cef581d001032443ca28393d4088c
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-20 15:36:25 +0000

    DONE LAST DIGIT

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Function to check for lowercase alphabets.
+ * @n: integar to check
+ *
+ * Return: (rem)
+*/
+
+int print_last_digit(int n)
+{
+	int rem;
+
+	if (n > 0)
+		rem = n % 10;
+	else if (n < 0)
+		rem = (-n) % 10;
+	else
+		rem = 0;
+	_putchar(rem + '0');
+	return (rem);
+}
