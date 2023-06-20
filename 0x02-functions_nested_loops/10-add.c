a4423c72b405734d65acbe788d65eab4292dbe8e
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-20 16:35:16 +0000

    DONE Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Function to add to integars.
+ * @a: number1 to be added.
+ * @b: number2 to be added.
+ *
+ * Return: result of (a) + (b).
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
