21dbccd9a13c377eee11da0aba99a0ce0f468563
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-22 15:32:58 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 /**
- * fizz_buzz - Function to check for multiples of 5 or 3 or both.
+ * main - Function to check for multiples of 5 or 3 or both.
+ *
+ * Description: To print Fizz buzz instead of multiples of 3 or 5
  *
  * Return: always 0 (success)
  */
