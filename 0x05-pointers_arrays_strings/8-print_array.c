b9a7613b453849d80a0d3446115d2510826cd421
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-27 14:59:16 +0000

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - Function to reset a number to 98.
+ * @a: Address.
+ * @n: size of arr.
+*/
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+		else
+			putchar('\n');
+	}
+}
