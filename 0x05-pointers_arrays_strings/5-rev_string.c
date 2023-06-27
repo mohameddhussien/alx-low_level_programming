dfcb6e46c9c323569cc6a854967e6d8fe8f565b2
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-27 03:16:27 +0000

    reverse a str

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - Function to reverse a string.
+ * @s: Address parameter.
+*/
+void rev_string(char *s)
+{
+	int i = 0, SI, temp;
+
+	while (s[i] != 0)
+		++i;
+	SI = --i;
+	i = 0;
+	while (i <= SI / 2)
+	{
+		temp = s[i];
+		s[i] = s[SI - i];  
+		s[SI - i] = temp;
+		i++;
+	}
+}
