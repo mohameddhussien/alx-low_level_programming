453f48c196c3f81b270c8b20c5d71af38af8be08
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-27 03:23:11 +0000

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - Function to reset a number to 98.
+ * @str: Address parameter.
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		_putchar(s[i]);
+		i += 2;
+	}
+}
