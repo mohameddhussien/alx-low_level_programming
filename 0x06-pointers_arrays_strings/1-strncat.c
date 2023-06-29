fa652b577e93abe0f239272bfff14a9e51fb7c3b
Mohamed Hussien <b.moody2004@gmail.com>
2023-06-29 16:58:25 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i, n1 = strlen(dest), n2 = strlen(src);
+	int i, n1 = strlen(dest), n2 = strlen(src);
 
-	for (i = 0; i < ; i++)
-		dest[n1 + i] = src[i];
-	if (n2 < n)
-		dest[n1 + n2] = '\0';
+	if (n < n2)
+		for (i = 0; i < n; i++)
+			dest[n1 + i] = src[i];
+	else
+	{
+		for (i = 0; i < n2; i++)
+			dest[n1 + i] = src[i];
+			dest[n1 + n2] = '\0';
+	}
 	return (dest);
 }
