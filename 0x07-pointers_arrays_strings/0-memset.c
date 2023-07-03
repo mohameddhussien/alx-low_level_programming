fe8dd78f83f0fe1eeb767e5515a9d23450dd3758
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-03 16:13:19 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,18 +11,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0, size = 0;
+	unsigned int i;
 
-	while (s[i] != '\0')
-	{
-		size++;
-		i++;
-	}
-	if (n <= size)
-		for (i = 0; i < n; i++)
-			s[i] = b;
-	else
-		for (i = 0; i < size; i++)
-			s[i] = b;
+	for (i = 0; n > 0; i++, n--)
+		s[i] = b;
 	return (s);
 }
