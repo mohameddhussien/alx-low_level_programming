e137dfb447eff4c30509ed192019d590c7c82380
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-03 17:09:21 +0000

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,8 +12,8 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (j = 0; accept[j] != '\0'; j++)
-		for (i = 0; s[i] >= '\0'; i++)
+	for (j = 0; accept[j] >= '\0'; j++)
+		for (i = 0; s[i] != '\0'; i++)
 			if (accept[j] == s[i])
 				return (i + 1);
 	return (0);
