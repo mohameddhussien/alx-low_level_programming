acbef0942d515e1dbc52a96574376312e00e17e3
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-10 12:07:23 +0000

    argc - 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point.
+ * @argc: para1
+ * @argv: para2
+ *
+ * Description: desc.
+ *
+ * Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
