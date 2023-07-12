16d03e9ab636e58b7a5f1a1dd770d094308356d4
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-12 00:24:54 +0000

    malloc char *str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - Function name.
+ * @str: para1.
+ *
+ * Return: length of str.
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strdup - Function name.
+ * @str: para1.
+ *
+ * Return: char.
+ */
+
+char *_strdup(char *str)
+{
+	int j = 0, size = _strlen(str);
+	char *ptr = (char *) malloc(size * sizeof(char));
+
+	if (str == NULL)
+                return (NULL);
+	for (j = 0; j < size; j++)
+		if (str[j] != '\0')
+			ptr[j] = str[j];
+	return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _strlen(char *str);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
