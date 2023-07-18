f5130ec36f113c2fe198c6bd2f4958ca322856ed
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-18 22:49:40 +0000

    struct Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
