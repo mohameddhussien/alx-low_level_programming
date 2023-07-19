4f163a8ca7a0f73f946f574cfd0871eb9b488c5c
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-19 14:41:20 +0000

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -23,6 +23,6 @@ int main(int argc, char **argv)
 	if (b < 0)
 		printf("Error\n"), exit(2);
 	while (b--)
-		printf("%02hhx%s", *p++, b ? " " : '\n');
+		printf("%02hhx%s", *p++, b ? " " : "\n");
 	return (0);
 }
