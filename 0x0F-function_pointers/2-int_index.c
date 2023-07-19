bef6b0e7744872d0dd96853f0fdaef58247bfaeb
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-19 12:55:44 +0000

    compare function pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function name.
+ * @array: array of numbers..
+ * @size: size of array.
+ * @cmp: print in digit or hexadecimal
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int found_index = -1, i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				found_index = i;
+				break;
+			}
+		}
+	}
+	if (found_index == -1)
+	{
+		return (-1);
+	}
+	return (found_index);
+}
