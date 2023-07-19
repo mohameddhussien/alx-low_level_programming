c7b491588bddc8be4bda14439806e8c0db35a5bc
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-19 12:34:18 +0000

    action Function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function name.
+ * @name: name to print.
+ * @f: uppercase or as it is.
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action && array)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
