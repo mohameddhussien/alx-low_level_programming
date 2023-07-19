111704564f7a042544abc0d0febe3695889af6d7
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-19 06:48:45 +0000

    print Dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-		printf("%f\n", (*d).age);
+		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 	}
 }
