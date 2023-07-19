e467310df0b296a51e64cc5d211dcc13aaaff907
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-19 12:26:14 +0000

    print_name Function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function name.
+ * @name: name to print.
+ * @f: uppercase or as it is.
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
