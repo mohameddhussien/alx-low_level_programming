4f739c511a7e5d4db0cbe04d92005b53e438ea70
Mohamed Hussien <b.moody2004@gmail.com>
2023-07-19 14:25:06 +0000

    add sub mul div mod

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: count
+ * @argv: values
+ *
+ * Description: main function.
+ *
+ * Return: 0 or 1
+ */
+
+int main (int argc, char **argv)
+{
+	int (*func_call)(int, int), a, b;
+
+	if (argc != 4)
+	{
+		printf("Error1\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	func_call = get_op_func(argv[2]);
+	if (!func_call)
+	{
+		printf("Error2\n");
+		exit(99);
+	}
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error3\n");
+		exit(100);
+	}
+	printf("%d\n", func_call(a, b));
+	return (0);
+}
